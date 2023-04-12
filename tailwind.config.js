2236f0d590036feab96f526306fae50a64f21f75
Pusaka Manggala <pusakamanggala@gmail.com>
2023-04-12 12:52:52 +0700

    add : primary and secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "primary-color": "#ec161e",
+        "secondary-color": "#c20000",
+      },
       backgroundImage: {
         "login-background": "url('/src/img/loginBG.jpg')",
       },
