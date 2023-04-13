b6d42fde7aa3e4b27cb16667d794fa3d6680405c
Pusaka Manggala <pusakamanggala@gmail.com>
2023-04-13 15:08:36 +0700

    add : route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import Login from "./pages/Login";
+import DashboardLayout from "./layout/DashboardLayout";
+import UserAccount from "./pages/UserAccount";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/beranda" element={<DashboardLayout></DashboardLayout>} />
+        <Route
+          path="/pengguna"
+          element={
+            <DashboardLayout>
+              <UserAccount />
+            </DashboardLayout>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
