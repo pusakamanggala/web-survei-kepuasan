aeaa86aeb78077b629b8824f180c29999b6a7082
Pusaka Manggala <pusakamanggala@gmail.com>
2023-04-13 15:10:52 +0700

    update : color

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { useState } from "react";
 import "tailwindcss/tailwind.css";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [selectedRole, setSelectedRole] = useState("Mahasiswa");
+  const navigate = useNavigate();
 
   return (
     <>
       <div className="flex h-screen bg-gray-100">
         {/* left panel */}
-        <div className="w-full md:w-1/2 md:block hidden bg-login-background bg-cover">
+        <div className="w-full md:w-1/2 md:block hidden bg-login-background bg-cover ">
           <div className="flex flex-col justify-center h-full pl-10 text-white">
             <h1 className="text-5xl font-bold ">Selamat Datang !</h1>
             <h2 className="text-xl">
@@ -24,20 +26,20 @@ function Login() {
         <div className="w-full md:w-1/2 bg-white px-10 py-10 flex flex-col justify-center">
           <form>
             <div className="text-center mb-10">
-              <h1 className="text-3xl font-bold">
+              <h1 className="text-3xl font-bold text-primary-color">
                 Login Sebagai {selectedRole}
               </h1>
             </div>
-            <div className="mb-5">
+            <div className="mb-5 ">
               <label
                 htmlFor="role"
-                className="block text-gray-700 font-bold mb-2"
+                className="block  font-bold mb-2 text-secondary-color"
               >
                 Siapa Kamu ?
               </label>
               <select
                 id="role"
-                className="border-2 border-gray-400 p-2 w-full rounded-md "
+                className="border-2  p-2 w-full rounded-md "
                 value={selectedRole}
                 onChange={(event) => setSelectedRole(event.target.value)}
               >
@@ -47,11 +49,8 @@ function Login() {
                 <option value="Alumni">Alumni</option>
               </select>
             </div>
-            <div className="mb-5">
-              <label
-                htmlFor="username"
-                className="block text-gray-700 font-bold mb-2"
-              >
+            <div className="mb-5 text-secondary-color">
+              <label htmlFor="username" className="block font-bold mb-2">
                 {selectedRole === "Admin" || selectedRole === "Dosen"
                   ? "NPM"
                   : "NIM"}
@@ -59,23 +58,20 @@ function Login() {
               <input
                 type="text"
                 id="username"
-                className="border-2 border-gray-400 p-2 w-full rounded-md"
+                className="border-2  p-2 w-full rounded-md"
                 value={username}
                 onChange={(event) => setUsername(event.target.value)}
                 autoComplete="username"
               />
             </div>
-            <div className="mb-5">
-              <label
-                htmlFor="password"
-                className="block text-gray-700 font-bold mb-2"
-              >
+            <div className="mb-5 text-secondary-color">
+              <label htmlFor="password" className="block font-bold mb-2">
                 Password
               </label>
               <input
                 type="password"
                 id="password"
-                className="border-2 border-gray-400 p-2 w-full rounded-md"
+                className="border-2  p-2 w-full rounded-md"
                 value={password}
                 onChange={(event) => setPassword(event.target.value)}
                 autoComplete="current-password"
@@ -84,7 +80,8 @@ function Login() {
             <div className="flex justify-end items-center">
               <button
                 type="submit"
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md"
+                className="bg-primary-color hover:bg-secondary-color text-white font-bold py-2 px-4 rounded-md"
+                onClick={navigate("/beranda")}
               >
                 Login
               </button>
