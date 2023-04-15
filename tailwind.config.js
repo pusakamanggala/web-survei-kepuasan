67aeba274414853680743948b34d55bf639b3e5b
Pusaka Manggala <pusakamanggala@gmail.com>
2023-04-15 15:12:31 +0700

    add : new scale

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      scale: {
+        102: "1.02",
+      },
       colors: {
         "primary-color": "#ec161e",
         "secondary-color": "#c20000",
