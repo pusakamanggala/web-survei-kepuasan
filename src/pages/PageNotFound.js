0f9208b8ebb026c210c6aee83b1127862e25e1ce
Pusaka Manggala <pusakamanggala@gmail.com>
2023-04-15 15:15:28 +0700

    add : 404 page

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PageNoutFoundImage from "../img/404.jpg";
+import { useNavigate } from "react-router-dom";
+
+const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-screen bg-white flex flex-col justify-center">
+      <img
+        src={PageNoutFoundImage}
+        alt="404 Page Not Found"
+        className="h-1/2 mx-auto "
+      />
+
+      <button
+        onClick={() => navigate("/beranda")}
+        className="bg-primary-color w-fit mx-auto p-2 rounded-md shadow-md"
+      >
+        <h1 className="text-center font-bold text-xl  cursor-pointer text-white">
+          Go Back
+        </h1>
+      </button>
+      <a
+        className="text-center text-sm font-light absolute bottom-2 right-2"
+        href="https://www.freepik.com/free-vector/error-404-concept-illustration_7741849.htm#query=404&position=1&from_view=search&track=sph"
+      >
+        Image by storyset on Freepik
+      </a>
+    </div>
+  );
+};
+
+export default PageNotFound;
