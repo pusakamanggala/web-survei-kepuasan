221de1d5e77e4d43b302a1c3c75e0cab5c4b96ea
Pusaka Manggala <pusakamanggala@gmail.com>
2023-04-15 15:14:40 +0700

    update : bg color

--- src/layout/DashboardLayout.js
+++ src/layout/DashboardLayout.js
@@ -93,7 +93,7 @@ const DashboardLayout = ({ children }) => {
             </div>
           </div>
         </header>
-        <main className="flex-1 overflow-y-auto bg-gray-200">
+        <main className="flex-1 overflow-y-auto bg-gray-100">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
             {children}
           </div>
