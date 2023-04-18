90dbef6924e2483cdbb59cb08c4c6b0f0a7dd056
Pusaka Manggala <pusakamanggala@gmail.com>
2023-04-18 15:03:45 +0700

    add : react query

--- src/App.js
+++ src/App.js
@@ -5,31 +5,36 @@ import UserAccount from "./pages/UserAccount";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/beranda"
-          element={
-            <DashboardLayout>
-              <Home />
-            </DashboardLayout>
-          }
-        />
-        <Route
-          path="/pengguna"
-          element={
-            <DashboardLayout>
-              <UserAccount />
-            </DashboardLayout>
-          }
-        />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/beranda"
+            element={
+              <DashboardLayout>
+                <Home />
+              </DashboardLayout>
+            }
+          />
+          <Route
+            path="/pengguna"
+            element={
+              <DashboardLayout>
+                <UserAccount />
+              </DashboardLayout>
+            }
+          />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
