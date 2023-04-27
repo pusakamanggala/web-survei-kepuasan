62f4e4e09446b05d168b6bac6f64e9d0046715ec
Pusaka Manggala <pusakamanggala@gmail.com>
2023-04-27 15:53:09 +0700

    update : navlink active/inactive conditions

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
   const [subMenu, setSubMenu] = useState(false);
+  const location = useLocation();
 
   return (
-    <nav className="bg-primary-color w-64 px-2 pt-4 pb-8 text-white z-50">
+    <div className="bg-primary-color w-64 px-2 pt-4 pb-8 text-white">
       <div className="flex justify-center">
         <span className="text-white text-3xl font-semibold py-6">Logo</span>
       </div>
@@ -33,8 +34,14 @@ const Sidebar = () => {
           <h1>Beranda</h1>
         </NavLink>
         <NavLink
-          to="/pengguna"
-          className={({ isActive }) => (isActive ? "active" : "inActive")}
+          to="/pengguna/mahasiswa"
+          className={({ isActive }) =>
+            isActive ||
+            location.pathname === "/pengguna/dosen" ||
+            location.pathname === "/pengguna/alumni"
+              ? "active"
+              : "inActive"
+          }
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +57,7 @@ const Sidebar = () => {
               d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
             />
           </svg>
-          <h1>Akun Pengguna</h1>
+          <h1>Pengguna</h1>
         </NavLink>
         <button
           className="p-5 flex w-full focus:bg-red-700 items-center"
@@ -181,9 +188,8 @@ const Sidebar = () => {
           <h1>Sign Out</h1>
         </button>
       </>
-
       {/* sidebar menu */}
-    </nav>
+    </div>
   );
 };
 
