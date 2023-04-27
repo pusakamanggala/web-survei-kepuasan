7acbc50094e253495ecded7b5a674ab3cf100be4
Pusaka Manggala <pusakamanggala@gmail.com>
2023-04-27 15:47:20 +0700

    add : role parameter to path

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
             }
           />
           <Route
-            path="/pengguna"
+            path="/pengguna/:role"
             element={
               <DashboardLayout>
                 <UserAccount />
