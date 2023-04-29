b54a2e99f04a30615b0b84614442f47fc5d4351a
Pusaka Manggala <pusakamanggala@gmail.com>
2023-04-29 12:34:36 +0700

    add : graduate student funct & paginations update

--- src/components/StudentTable.js
+++ src/components/StudentTable.js
@@ -3,11 +3,19 @@ import useFetchStudents from "../hooks/useFetchStudents";
 import useFetchUserById from "../hooks/useFetchUserById";
 import useFetchUserByName from "../hooks/useFetchUserByName";
 import { useParams } from "react-router-dom";
+import GraduateStudent from "./GraduateStudent";
 
 const StudentTable = ({ keyword, angkatan }) => {
   // Set initial state values for sorting
   const [isAsc, setIsAsc] = useState(true);
   const [orderBy, setOrderBy] = useState("nim");
+
+  const [showModal, setShowModal] = useState(false); // state to open and close graduate student modal
+
+  const [pageNumber, setPageNumber] = useState(1); // Set initial page number to 1
+  const [pageSize, setPageSize] = useState(5); // Set initial page size to 5
+
+  const [selectedStudent, setSelectedStudent] = useState(null); // state to store selected student data
 
   // get role from url
   const { role } = useParams();
@@ -22,6 +30,27 @@ const StudentTable = ({ keyword, angkatan }) => {
       studentData.data.length !== 1 &&
       keyword === ""
     );
+  };
+
+  // Define two functions to handle pagination
+  const handleNextPage = () => {
+    setPageNumber((prevPageNumber) => prevPageNumber + 1);
+  };
+  const handlePrevPage = () => {
+    setPageNumber((prevPageNumber) => prevPageNumber - 1);
+  };
+  const handleFirstPage = () => {
+    setPageNumber(1);
+  };
+  const handleLastPage = (totalPage) => {
+    setPageNumber(totalPage);
+  };
+
+  // Define function to handle page size change
+  const handlePageSizeChange = (event) => {
+    const newSize = parseInt(event.target.value);
+    setPageSize(newSize);
+    setPageNumber(1); // Reset page number to 1 when page size is changed
   };
 
   // Declare variables for fetching data (will be assigned based on conditions)
@@ -43,8 +72,8 @@ const StudentTable = ({ keyword, angkatan }) => {
     fetchArgs = {
       sortBy: isAsc ? "asc" : "desc", // Set the sorting order
       orderBy: orderBy, // Set the field to sort by
-      limit: 10, // Set the number of items per page
-      page: 1, // Set the initial page number
+      limit: pageSize, // Set the number of items per page
+      page: pageNumber, // Set the initial page number
       angkatan: angkatan, // Set the angkatan filter
     };
   }
@@ -86,171 +115,285 @@ const StudentTable = ({ keyword, angkatan }) => {
     );
 
   return (
-    <table className="w-full border-2 ">
-      <thead>
-        <tr>
-          <th
-            scope="col"
-            className="text-sm font-medium bg-secondary-color text-white px-6 py-4 text-left cursor-pointer "
-            title="Urutkan berdasarkan NIM"
-            onClick={() => {
-              setIsAsc(!isAsc);
-              setOrderBy("nim");
-            }}
-          >
-            NIM
-            {shouldShowArrow("nim") && (
-              <svg
-                className="inline-block h-3 w-3 stroke-white text-white ml-2"
-                viewBox="0 0 8 8"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill="white"
-                  d={isAsc ? "M8 6L4 2 0 6" : "M0 2l4 4 4-4"}
-                ></path>
-              </svg>
-            )}
-          </th>
+    <div>
+      <div className="flex justify-between text-secondary-color font-medium mx-1">
+        {/* To show total users */}
+        <h1>Total Mahasiswa : {studentData.totalRecords}</h1>
+        <h1>
+          Halaman : {pageNumber} / {studentData.totalPage}
+        </h1>
+      </div>
 
-          <th
-            scope="col"
-            className="text-sm font-medium text-white bg-secondary-color px-6 py-4 text-left cursor-pointer"
-            title="Urutkan berdasarkan nama"
-            onClick={() => {
-              setIsAsc(!isAsc);
-              setOrderBy("nama");
-            }}
-          >
-            Nama
-            {shouldShowArrow("nama") && (
-              <svg
-                className="inline-block h-3 w-3 stroke-white text-white ml-2"
-                viewBox="0 0 8 8"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill="white"
-                  d={isAsc ? "M8 6L4 2 0 6" : "M0 2l4 4 4-4"}
-                ></path>
-              </svg>
-            )}
-          </th>
+      {/* Table */}
+      <table className="w-full border-2 ">
+        <thead>
+          <tr>
+            <th
+              scope="col"
+              className="text-sm font-medium bg-secondary-color text-white px-6 py-4 text-left cursor-pointer "
+              title="Urutkan berdasarkan NIM"
+              onClick={() => {
+                setIsAsc(!isAsc);
+                setOrderBy("nim");
+              }}
+            >
+              NIM
+              {shouldShowArrow("nim") && (
+                <svg
+                  className="inline-block h-3 w-3 stroke-white text-white ml-2"
+                  viewBox="0 0 8 8"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill="white"
+                    d={isAsc ? "M8 6L4 2 0 6" : "M0 2l4 4 4-4"}
+                  ></path>
+                </svg>
+              )}
+            </th>
 
-          <th
-            scope="col"
-            className="text-sm font-medium text-white bg-secondary-color px-6 py-4 cursor-pointer "
-            title="Urutkan berdasarkan angkatan"
-            onClick={() => {
-              setIsAsc(!isAsc);
-              setOrderBy("angkatan");
-            }}
-          >
-            Angkatan
-            {shouldShowArrow("angkatan") && (
-              <svg
-                className="inline-block h-3 w-3 stroke-white text-white ml-2"
-                viewBox="0 0 8 8"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill="white"
-                  d={isAsc ? "M8 6L4 2 0 6" : "M0 2l4 4 4-4"}
-                ></path>
-              </svg>
-            )}
-          </th>
-          <th
-            scope="col"
-            className="text-sm font-medium text-white bg-secondary-color px-6 py-4 text-left"
-          >
-            Telepon
-          </th>
-          <th
-            scope="col"
-            className="text-sm font-medium text-white bg-secondary-color px-6 py-4"
-          >
-            Status
-          </th>
-          <th
-            scope="col"
-            className="text-sm font-medium text-white bg-secondary-color px-6 py-4 text-center"
-          >
-            Actions
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {Array.isArray(studentData.data) ? (
-          studentData.data.map((res, index) => (
-            // print data to table
-            <tr className="bg-gray-100 border-b" key={index}>
+            <th
+              scope="col"
+              className="text-sm font-medium text-white bg-secondary-color px-6 py-4 text-left cursor-pointer"
+              title="Urutkan berdasarkan nama"
+              onClick={() => {
+                setIsAsc(!isAsc);
+                setOrderBy("nama");
+              }}
+            >
+              Nama
+              {shouldShowArrow("nama") && (
+                <svg
+                  className="inline-block h-3 w-3 stroke-white text-white ml-2"
+                  viewBox="0 0 8 8"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill="white"
+                    d={isAsc ? "M8 6L4 2 0 6" : "M0 2l4 4 4-4"}
+                  ></path>
+                </svg>
+              )}
+            </th>
+
+            <th
+              scope="col"
+              className="text-sm font-medium text-white bg-secondary-color px-6 py-4 cursor-pointer "
+              title="Urutkan berdasarkan angkatan"
+              onClick={() => {
+                setIsAsc(!isAsc);
+                setOrderBy("angkatan");
+              }}
+            >
+              Angkatan
+              {shouldShowArrow("angkatan") && (
+                <svg
+                  className="inline-block h-3 w-3 stroke-white text-white ml-2"
+                  viewBox="0 0 8 8"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill="white"
+                    d={isAsc ? "M8 6L4 2 0 6" : "M0 2l4 4 4-4"}
+                  ></path>
+                </svg>
+              )}
+            </th>
+            <th
+              scope="col"
+              className="text-sm font-medium text-white bg-secondary-color px-6 py-4 text-left"
+            >
+              Telepon
+            </th>
+            <th
+              scope="col"
+              className="text-sm font-medium text-white bg-secondary-color px-6 py-4"
+            >
+              Status
+            </th>
+            <th
+              scope="col"
+              className="text-sm font-medium text-white bg-secondary-color px-6 py-4 text-center"
+            >
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(studentData.data) ? (
+            studentData.data.map((res, index) => (
+              // print data to table
+              <tr className="bg-gray-100 border-b" key={index}>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {res.nim}
+                </td>
+                <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap">
+                  {res.nama}
+                </td>
+                <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap text-center">
+                  {res.angkatan}
+                </td>
+                <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap">
+                  {res.telepon}
+                </td>
+                <td
+                  className={`text-sm font-medium px-6 py-4 whitespace-nowrap text-center ${
+                    res.status === "Aktif"
+                      ? "text-red-500"
+                      : res.status === "Lulus"
+                      ? "text-blue-500"
+                      : "text-green-500"
+                  }`}
+                >
+                  {res.status}
+                </td>
+                {/* Actions Buttons */}
+                <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap text-center w-auto">
+                  <button
+                    className="bg-yellow-400 text-white font-medium px-4 py-2 rounded-md shadow-md"
+                    onClick={() => {
+                      setSelectedStudent(res);
+                      setShowModal(true);
+                    }}
+                  >
+                    Luluskan
+                  </button>
+                  {/* GraduateStudent component */}
+                  {showModal && (
+                    <GraduateStudent
+                      nama={selectedStudent.nama}
+                      nim={selectedStudent.nim}
+                      angkatan={selectedStudent.angkatan}
+                      status={selectedStudent.status}
+                      setIsShow={setShowModal}
+                    />
+                  )}
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr className="bg-gray-100 border-b">
               <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                {res.nim}
+                {studentData.data.nim}
               </td>
               <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap">
-                {res.nama}
+                {studentData.data.nama}
               </td>
               <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap text-center">
-                {res.angkatan}
+                {studentData.data.angkatan}
               </td>
               <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap">
-                {res.telepon}
+                {studentData.data.telepon}
               </td>
               <td
                 className={`text-sm font-medium px-6 py-4 whitespace-nowrap text-center ${
-                  res.status === "Aktif"
+                  studentData.data.status === "Aktif"
                     ? "text-red-500"
-                    : res.status === "Lulus"
+                    : studentData.data.status === "Lulus"
                     ? "text-blue-500"
                     : "text-green-500"
                 }`}
               >
-                {res.status}
+                {studentData.data.status}
               </td>
               {/* Actions Buttons */}
               <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap text-center w-auto">
-                <button className="bg-yellow-400 text-white font-medium px-4 py-2 rounded-md shadow-md">
-                  Edit
+                <button
+                  className="bg-yellow-400 text-white font-medium px-4 py-2 rounded-md shadow-md"
+                  onClick={() => {
+                    setSelectedStudent(studentData.data);
+                    setShowModal(true);
+                  }}
+                >
+                  Luluskan
                 </button>
+                {/* GraduateStudent component */}
+                {showModal && (
+                  <GraduateStudent
+                    nama={selectedStudent.nama}
+                    nim={selectedStudent.nim}
+                    angkatan={selectedStudent.angkatan}
+                    status={selectedStudent.status}
+                    setIsShow={setShowModal}
+                  />
+                )}
               </td>
             </tr>
-          ))
-        ) : (
-          <tr className="bg-gray-100 border-b">
-            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-              {studentData.data.nim}
-            </td>
-            <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap">
-              {studentData.data.nama}
-            </td>
-            <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap text-center">
-              {studentData.data.angkatan}
-            </td>
-            <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap">
-              {studentData.data.telepon}
-            </td>
-            <td
-              className={`text-sm font-medium px-6 py-4 whitespace-nowrap text-center ${
-                studentData.data.status === "Aktif"
-                  ? "text-red-500"
-                  : studentData.data.status === "Lulus"
-                  ? "text-blue-500"
-                  : "text-green-500"
-              }`}
-            >
-              {studentData.data.status}
-            </td>
-            {/* Actions Buttons */}
-            <td className="text-sm text-gray-900 font-base px-6 py-4 whitespace-nowrap text-center w-auto">
-              <button className="bg-yellow-400 text-white font-medium px-4 py-2 rounded-md shadow-md">
-                Edit
+          )}
+        </tbody>
+      </table>
+      <div className="flex justify-between items-center pt-6 w-full">
+        {/* button to set how much data is displayed  */}
+        <div className="flex text-primary-color font-semibold ml-2">
+          {studentData && studentData.totalRecords > 5 ? (
+            <>
+              <h1>Tampilkan</h1>
+              <select
+                name=""
+                id=""
+                className="text-secondary-color"
+                value={pageSize}
+                onChange={handlePageSizeChange}
+              >
+                <option value="5">5</option>
+                <option value="10">10</option>
+              </select>
+            </>
+          ) : null}
+        </div>
+        {studentData && studentData.totalPage !== 1 && keyword === "" ? (
+          <div>
+            {pageNumber !== 1 ? (
+              <button
+                onClick={handleFirstPage}
+                disabled={pageNumber === 1}
+                className={`font-bold py-2 px-4 rounded mr-2 text-white ${
+                  pageNumber === 1
+                    ? "bg-red-400 "
+                    : "bg-red-600 hover:bg-red-700 "
+                } `}
+              >
+                First
               </button>
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
+            ) : null}
+
+            <button
+              onClick={handlePrevPage}
+              disabled={pageNumber === 1}
+              className={`font-bold py-2 px-4 rounded mr-2 text-white ${
+                pageNumber === 1
+                  ? "bg-red-400 "
+                  : "bg-red-600 hover:bg-red-700 "
+              } `}
+            >
+              Prev
+            </button>
+            <button
+              onClick={handleNextPage}
+              disabled={pageNumber === studentData.totalPage}
+              className={`font-bold py-2 px-4 rounded mr-2 text-white ${
+                pageNumber === studentData.totalPage
+                  ? "bg-red-400 "
+                  : "bg-red-600 hover:bg-red-700 "
+              } `}
+            >
+              Next
+            </button>
+            <button
+              onClick={() => handleLastPage(studentData.totalPage)}
+              disabled={pageNumber === studentData.totalPage}
+              className={`font-bold py-2 px-4 rounded mr-2 text-white ${
+                pageNumber === studentData.totalPage
+                  ? "bg-red-400 "
+                  : "bg-red-600 hover:bg-red-700 "
+              } `}
+            >
+              Last
+            </button>
+          </div>
+        ) : null}
+      </div>
+    </div>
   );
 };
 
