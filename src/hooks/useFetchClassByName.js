0aa471a26bab057208754f40692f50d5ba34eab2
Pusaka Manggala <pusakamanggala@gmail.com>
2023-05-04 14:57:36 +0700

    add : hook to fetch class by name

---
+++ src/hooks/useFetchClassByName.js
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+
+const useFetchClassByName = ({ keyword }) => {
+  const fetchClass = async () => {
+    // Define async function to fetch class data
+    const response = await fetch(
+      `http://localhost:8000/kelas/suggest?query=${keyword}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch class data");
+    }
+    const data = await response.json();
+    return data;
+  };
+  // Use useQuery hook to fetch and cache class data
+  return useQuery(["class", keyword], fetchClass, {
+    refetchOnWindowFocus: false,
+    staleTime: 60000, // 1 minute
+    cacheTime: 3600000, // 1 hour
+  });
+};
+
+export default useFetchClassByName;
