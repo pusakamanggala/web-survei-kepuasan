04be01d6ede4e818ff9d338ce8d0770a1e01db62
Pusaka Manggala <pusakamanggala@gmail.com>
2023-05-04 14:58:03 +0700

    add : hook to fetch classes

---
+++ src/hooks/useFetchClasses.js
@@ -0,0 +1,24 @@
+import { useQuery } from "react-query";
+
+const useFetchClasses = ({ limit, page }) => {
+  // Define async function to fetch courses data
+  const fetchClasses = async () => {
+    const response = await fetch(
+      `http://localhost:8000/kelas?limit=${limit}&page=${page}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch classes data");
+    }
+    const data = await response.json();
+    return data;
+  };
+
+  // Use useQuery hook to fetch and cache classes data
+  return useQuery(["classes", limit, page], fetchClasses, {
+    refetchOnWindowFocus: false,
+    staleTime: 60000, // 1 minute
+    cacheTime: 3600000, // 1 hour
+  });
+};
+
+export default useFetchClasses;
