2711730b739355d845c918e057025d634e5cb3d6
Pusaka Manggala <pusakamanggala@gmail.com>
2023-05-15 16:02:10 +0700

    add : onClick, bgColor, and textColor props

--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
-const ClassCard = ({ kelas }) => {
-  const navigate = useNavigate();
+const ClassCard = ({
+  kelas,
+  onClick,
+  bgColor = "bg-white",
+  textColor = "text-secondary-color",
+}) => {
   return (
     <div
-      className="bg-white rounded-lg overflow-hidden shadow-md p-3 flex justify-between items-center cursor-pointer hover:scale-102 transition-all duration-300"
-      onClick={() => navigate(`/kelas/${kelas.id_kelas}`)}
+      className={` " rounded-lg overflow-hidden shadow-md p-3 flex justify-between items-center cursor-pointer hover:scale-102 transition-all duration-300" ${bgColor}`}
+      onClick={onClick}
     >
       <div>
-        <h1 className="text-secondary-color font-bold text-lg">
+        <h1 className={`"text-secondary-color font-bold text-lg" ${textColor}`}>
           {kelas.nama_kelas}
         </h1>
         <h1>
           Pengampu :{" "}
-          <span className="text-secondary-color font-semibold ">
+          <span className={`" font-semibold " ${textColor}`}>
             {kelas.nama_dosen}
           </span>
         </h1>
