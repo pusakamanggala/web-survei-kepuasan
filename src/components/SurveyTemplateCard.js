89e0f8c02397cf91418d628b9a926382bf9db7c5
Pusaka Manggala <pusakamanggala@gmail.com>
2023-05-15 16:03:09 +0700

    add : bgColor and textColor props

--- src/components/SurveyTemplateCard.js
+++ src/components/SurveyTemplateCard.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-const QuestionsCard = ({ template, onClick }) => {
+const SurveyTemplateCard = ({
+  template,
+  onClick,
+  bgColor = "bg-primary-color",
+  textColor = "text-white",
+}) => {
   return (
     <div
-      className="rounded-lg overflow-hidden shadow-md p-3 flex justify-between items-center cursor-pointer hover:scale-102 transition-all duration-300 bg-primary-color text-white"
+      className={` rounded-lg overflow-hidden shadow-md p-3 flex justify-between items-center cursor-pointer hover:scale-102 transition-all duration-300 ${bgColor} ${textColor}`}
       onClick={onClick}
     >
-      <div>
+      <div className={`${textColor}`}>
         <h1 className=" font-bold">{template.namaTemplate}</h1>
         <h1> Tipe Survei : {template.role}</h1>
       </div>
@@ -14,4 +19,4 @@ const QuestionsCard = ({ template, onClick }) => {
   );
 };
 
-export default QuestionsCard;
+export default SurveyTemplateCard;
