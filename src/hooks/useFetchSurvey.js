409c409b26b41731fdd01d897ea18811439867cc
Pusaka Manggala <pusakamanggala@gmail.com>
2023-05-25 14:55:25 +0700

    add : hook to fetch user survey

---
+++ src/hooks/useFetchSurvey.js
@@ -0,0 +1,27 @@
+import { useQuery } from "react-query";
+
+const useFetchSurvey = ({ role, id, autoFetch = true }) => {
+  const fetchSurvey = async () => {
+    // Define async function to fetch survey data
+    const response = await fetch(
+      `${process.env.REACT_APP_API_ENDPOINT}/survey?role=${role}&id=${id}`,
+      {
+        credentials: "include", // Include credentials in the request
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch survey data");
+    }
+    const data = await response.json();
+    return data;
+  };
+  // Use useQuery hook to fetch and cache survey data
+  return useQuery(["survey", role, id], fetchSurvey, {
+    refetchOnWindowFocus: false,
+    staleTime: 60000, // 1 minute
+    cacheTime: 3600000, // 1 hour
+    enabled: autoFetch, // to set auto fetch or not
+  });
+};
+
+export default useFetchSurvey;
