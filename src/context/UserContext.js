26b774c52adee70e5675946ac0996805d700567e
Pusaka Manggala <pusakamanggala@gmail.com>
2023-05-26 15:16:43 +0700

    add : survey state to set and get user survey data

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -8,14 +8,31 @@ export const UserProvider = ({ children }) => {
   const [userRole, setUserRole] = useState("");
   const [userId, setUserId] = useState(null);
 
+  // Initialize the survey state using the useState hook
+  const [survey, setSurvey] = useState(() => {
+    // Retrieve the stored survey from the browser's local storage
+    const storedSurvey = localStorage.getItem("survey");
+    // If a stored survey exists, parse it from JSON format
+    // Otherwise, set the initial value of survey to null
+    return storedSurvey ? JSON.parse(storedSurvey) : null;
+  });
+
   // Function to set the user role and id when login is successful
   const setUser = (role, id) => {
     setUserRole(role);
     setUserId(id);
   };
 
+  // Function to update the survey and store it in local storage
+  const updateSurvey = (newSurvey) => {
+    setSurvey(newSurvey);
+    localStorage.setItem("survey", JSON.stringify(newSurvey));
+  };
+
   return (
-    <UserContext.Provider value={{ userRole, userId, setUser }}>
+    <UserContext.Provider
+      value={{ userRole, userId, setUser, survey, setSurvey: updateSurvey }}
+    >
       {children}
     </UserContext.Provider>
   );
