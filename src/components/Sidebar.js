8b936dacf8d4d363cfbcf9c4472f615dc069fc7f
Pusaka Manggala <pusakamanggala@gmail.com>
2023-05-31 13:51:58 +0700

    add : survey history menu

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -265,6 +265,26 @@ const Sidebar = () => {
               </svg>
               <h1>Survei Saya</h1>
             </NavLink>
+            <NavLink
+              to="/survei-kepuasan/riwayat-survei"
+              className={({ isActive }) => (isActive ? "active" : "inActive")}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 mr-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                />
+              </svg>
+              <h1>Riwayat Survei</h1>
+            </NavLink>
           </div>
         )}
         {/*end of survei sub menu */}
