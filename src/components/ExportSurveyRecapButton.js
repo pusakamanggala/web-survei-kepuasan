2c4b91577ae0c33cf49cd7cb27c80ff132954872
Pusaka Manggala <pusakamanggala@gmail.com>
2023-06-07 15:35:04 +0700

    update : xlsx import

--- src/components/ExportSurveyRecapButton.js
+++ src/components/ExportSurveyRecapButton.js
@@ -1,11 +1,11 @@
 import React from "react";
-import * as XLSX from "xlsx";
+import { utils, writeFile } from "xlsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileExport } from "@fortawesome/free-solid-svg-icons";
 
 function ExportSurveyRecapButton({ tableIds }) {
   const handleExportExcel = () => {
-    const workbook = XLSX.utils.book_new();
+    const workbook = utils.book_new();
 
     tableIds.forEach((tableId) => {
       const tableElement = document.getElementById(tableId);
@@ -14,11 +14,11 @@ function ExportSurveyRecapButton({ tableIds }) {
         return;
       }
 
-      const worksheet = XLSX.utils.table_to_sheet(tableElement);
-      XLSX.utils.book_append_sheet(workbook, worksheet, tableId);
+      const worksheet = utils.table_to_sheet(tableElement);
+      utils.book_append_sheet(workbook, worksheet, tableId);
     });
 
-    XLSX.writeFile(workbook, "tables.xlsx");
+    writeFile(workbook, "tables.xlsx");
   };
 
   return (
@@ -28,7 +28,7 @@ function ExportSurveyRecapButton({ tableIds }) {
       className="flex items-center bg-primary-color shadow-md hover:bg-secondary-color text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
     >
       <h1 className="flex">
-        Ekpor <span className="hidden md:block ml-1">Rekap</span>
+        Export <span className="hidden md:block ml-1">Rekap</span>
       </h1>
       <FontAwesomeIcon icon={faFileExport} className="ml-2" />
     </button>
