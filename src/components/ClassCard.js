23b2c6ebeab47ddce9c512f1edb09a0cf4800ce7
Pusaka Manggala <pusakamanggala@gmail.com>
2023-07-17 13:51:14 +0700

    update : text color

--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -8,18 +8,13 @@ const ClassCard = ({
 }) => {
   return (
     <div
-      className={` " rounded-lg overflow-hidden shadow-md p-3 flex justify-between items-center cursor-pointer hover:scale-102 transition-all duration-300" ${bgColor}`}
+      className={` " rounded-lg overflow-hidden shadow-md p-3 flex justify-between items-center cursor-pointer hover:scale-102 transition-all duration-300" ${bgColor} ${textColor}`}
       onClick={onClick}
     >
       <div>
-        <h1 className={`"text-secondary-color font-bold text-lg" ${textColor}`}>
-          {kelas.nama_kelas}
-        </h1>
+        <h1 className="font-bold text-lg">{kelas.nama_kelas}</h1>
         <h1>
-          Pengampu :{" "}
-          <span className={`" font-semibold " ${textColor}`}>
-            {kelas.nama_dosen}
-          </span>
+          Pengampu : <span className="font-semibold">{kelas.nama_dosen}</span>
         </h1>
       </div>
     </div>
